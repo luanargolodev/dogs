d0df7a2ea7a2452e0b617056137ef8452d955875
Luan Argolo <ipsluan@icloud.com>
2022-04-18 17:58:49 -0300

    :sparkles: feat: criar o Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './styles.css';
+
+const Footer = () => {
+  return <div className={styles.footer}>Footer</div>;
+};
+
+export default Footer;
