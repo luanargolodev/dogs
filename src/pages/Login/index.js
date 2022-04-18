2d125d5a548044c047bccc8f5a37fc24992337a8
Luan Argolo <ipsluan@icloud.com>
2022-04-18 18:20:09 -0300

    :sparkles: feat: cadastramento das rotas

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="lost" element={<LoginPasswordLost />} />
+        <Route path="reset" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Login;
