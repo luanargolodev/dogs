abe9d7ac63d29710be3d24e052872022085b4a53
Luan Argolo <ipsluan@icloud.com>
2022-04-19 12:22:33 -0300

    :recycle: refactor: criar component useFetch

--- src/pages/Login/LoginCreate/index.js
+++ src/pages/Login/LoginCreate/index.js
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
+import Error from '../../../components/helper/Error';
 
 import useForm from '../../../hooks/useForm';
 import { USER_POST } from '../../../services/api';
 
 import { UserContext } from '../../../contexts/UserContext';
+import useFetch from '../../../hooks/useFetch';
 
 const LoginCreate = () => {
   const username = useForm();
@@ -14,6 +16,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -22,7 +25,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -33,7 +36,12 @@ const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="E-mail" type="email" name="email" {...email} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
