74d47d8eed61140ce504fc8526e1bdeb5c99e6cf
Luan Argolo <ipsluan@icloud.com>
2022-04-20 11:40:43 -0300

    :sparkles: feat: finalizar feed

---
+++ src/components/FeedPhotos/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import useFetch from '../../hooks/useFetch';
+import { PHOTOS_GET } from '../../services/api';
+import FeedPhotosItem from '../FeedPhotosItem';
+import Error from '../../helper/Error';
+import Loading from '../../helper/Loading';
+import styles from './styles.module.css';
+
+const FeedPhotos = () => {
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { json } = await request(url, options);
+      console.log(json);
+    }
+
+    fetchPhotos();
+  }, [request]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data) {
+    return (
+      <div>
+        <ul className={`${styles.feed} animeLeft`}>
+          {data.map((photo) => (
+            <FeedPhotosItem key={photo.id} photo={photo} />
+          ))}
+        </ul>
+      </div>
+    );
+  } else return null;
+};
+
+export default FeedPhotos;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Feed from '../../components/Feed';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="container mainContainer">
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
