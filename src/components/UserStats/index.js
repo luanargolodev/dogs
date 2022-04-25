5ca5d0f88216c2535ae764defd1826630699c32b
Luan Argolo <ipsluan@icloud.com>
2022-04-25 15:22:24 -0300

    :sparkles: feat: adicionar gráficos e lazy

--- src/components/UserStats/index.js
+++ src/components/UserStats/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { lazy, useEffect } from 'react';
 import Head from '../../helper/Head';
+import useFetch from '../../hooks/useFetch';
+import { STATS_GET } from '../../services/api';
+import Loading from '../../helper/Loading';
+import Error from '../../helper/Error';
+const UserStatsGraphs = lazy(() => import('../UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  } else return null;
 };
 
 export default UserStats;
--- src/services/api.js
+++ src/services/api.js
@@ -134,3 +134,15 @@ export function PASSWORD_RESET(body) {
     },
   };
 }
+
+export function STATS_GET(body) {
+  return {
+    url: API_URL + '/api/stats',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + window.localStorage.getItem('token'),
+      },
+    },
+  };
+}
