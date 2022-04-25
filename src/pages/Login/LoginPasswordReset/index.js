bed8f8ee5a40595a6bc257f52d333ef79540ebd9
Luan Argolo <ipsluan@icloud.com>
2022-04-25 10:56:14 -0300

    :sparkles: feat: finalizar envio de email ao esquecer senha

--- src/pages/Login/LoginPasswordReset/index.js
+++ src/pages/Login/LoginPasswordReset/index.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Button from '../../../components/Button';
+import Input from '../../../components/Input';
+
+import useForm from '../../../hooks/useForm';
+import useFetch from '../../../hooks/useFetch';
+import { PASSWORD_RESET } from '../../../services/api';
+import Error from '../../../helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPasswordReset = () => {
-  return <div>LoginPasswordReset</div>;
+  const [login, setLogin] = useState('');
+  const [key, setKey] = useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const login = params.get('login');
+    const key = params.get('key');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+      const { response } = await request(url, options);
+      if (response.ok) {
+        navigate('/login');
+      }
+    }
+  }
+
+  return (
+    <div>
+      <h1 className="title">Recupere sua conta</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Recuperando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
